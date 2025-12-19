f02f1a49561aee312355d3ae37c3dbed15a0d565
LeggyBoiTim <105779117+LeggyBoiTim@users.noreply.github.com>
2025-12-19 14:07:37 +0100

    Update store.js

--- src/domains/inventory/store.js
+++ src/domains/inventory/store.js
@@ -2,16 +2,16 @@ import { computed, ref } from "vue";
 
 // State
 const inventory = ref([
-    { id: 1, name: 'Brood', actualAmount: 5, minimumAmount: 1 },
-    { id: 2, name: 'Rundergehakt', actualAmount: 5, minimumAmount: 1 },
-    { id: 3, name: 'Avocado', actualAmount: 5, minimumAmount: 1 },
-    { id: 4, name: 'Chips', actualAmount: 5, minimumAmount: 1 },
-    { id: 5, name: 'Yoghurt', actualAmount: 5, minimumAmount: 1 },
-    { id: 6, name: 'Chocoladereep', actualAmount: 5, minimumAmount: 1 },
-    { id: 7, name: 'IJsthee', actualAmount: 5, minimumAmount: 1 },
-    { id: 8, name: 'Melk', actualAmount: 5, minimumAmount: 1 },
-    { id: 9, name: 'Sinaasappelsap', actualAmount: 5, minimumAmount: 1 },
-    { id: 10, name: 'Wijn', actualAmount: 5, minimumAmount: 1 },
+    { id: 1, name: 'Brood', actualAmount: 132, minimumAmount: 1 },
+    { id: 2, name: 'Rundergehakt', actualAmount: 7, minimumAmount: 1 },
+    { id: 3, name: 'Avocado', actualAmount: 37, minimumAmount: 1 },
+    { id: 4, name: 'Chips', actualAmount: 26, minimumAmount: 1 },
+    { id: 5, name: 'Yoghurt', actualAmount: 43, minimumAmount: 1 },
+    { id: 6, name: 'Chocoladereep', actualAmount: 87, minimumAmount: 1 },
+    { id: 7, name: 'IJsthee', actualAmount: 16, minimumAmount: 1 },
+    { id: 8, name: 'Melk', actualAmount: 91, minimumAmount: 1 },
+    { id: 9, name: 'Sinaasappelsap', actualAmount: 8, minimumAmount: 1 },
+    { id: 10, name: 'Wijn', actualAmount: 54, minimumAmount: 1 },
 ]);
 
 // Getters
