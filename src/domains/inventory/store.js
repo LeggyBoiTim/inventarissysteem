d65cee4a0a1568f60d332bbcfd80a25512e16b60
LeggyBoiTim <105779117+LeggyBoiTim@users.noreply.github.com>
2025-12-19 14:04:03 +0100

    Updated store.js and added store examples.

--- src/domains/inventory/store.js
+++ src/domains/inventory/store.js
@@ -2,7 +2,16 @@ import { computed, ref } from "vue";
 
 // State
 const inventory = ref([
-
+    { id: 1, name: 'Brood', actualAmount: 5, minimumAmount },
+    { id: 2, name: 'Rundergehakt', actualAmount: 5, minimumAmount },
+    { id: 3, name: 'Avocado', actualAmount: 5, minimumAmount },
+    { id: 4, name: 'Chips', actualAmount: 5, minimumAmount },
+    { id: 5, name: 'Yoghurt', actualAmount: 5, minimumAmount },
+    { id: 6, name: 'Chocoladereep', actualAmount: 5, minimumAmount },
+    { id: 7, name: 'IJsthee', actualAmount: 5, minimumAmount },
+    { id: 8, name: 'Melk', actualAmount: 5, minimumAmount },
+    { id: 9, name: 'Sinaasappelsap', actualAmount: 5, minimumAmount },
+    { id: 10, name: 'Wijn', actualAmount: 5, minimumAmount },
 ]);
 
 // Getters
